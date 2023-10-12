519dd4f90d76cf818e23bd3524b2014c7f18b720
bulldog3000 <128056980+bulldog3000@users.noreply.github.com>
2023-10-12 12:53:24 +0300

    Commited

---
+++ stage0-module4-conditions-task4-master/src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
@@ -0,0 +1,7 @@
+package school.mjc.stage0.conditions.task4;
+
+public class TernaryGreatestNumberPrinter {
+    public void printGreatest(int first, int second) {
+        int res = first > second ? first: second;
+    }
+}
---
+++ stage0-module4-conditions-task4-master/src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -0,0 +1,8 @@
+package school.mjc.stage0.conditions.task4;
+
+public class TernaryMaxNumberPrinter {
+    public void printGreatest(int first, int second, int third) {
+        int res;
+        res = first > second && first > third ? first : second > third ? second: third;
+    }
+}
